c4ba226680ab895ca12c1536f471bfed1308839e
Eric Doering <ericbenjamindoering@gmail.com>
2023-05-19 12:56:16 -0400

    setting up typescript types

--- eric_doering_website/sanity/sanity-utils.ts
+++ eric_doering_website/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { createClient, groq } from "next-sanity";
+import { Project } from "@/types/Project";
 
-export async function getProjects(){
+export async function getProjects(): Promise<Project[]> {
     const client = createClient({
         projectId: "xsvj9bid",
         dataset: "production",
