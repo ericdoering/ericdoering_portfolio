fd6dbf8579342b4927c89a9a028c486eb41b60dd
Eric Doering <ericbenjamindoering@gmail.com>
2023-05-27 12:32:36 -0400

    setup of pages

--- eric_doering_website/app/(site)/layout.tsx
+++ eric_doering_website/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.css'
 import { Inter } from 'next/font/google'
 import background from "../assets/background.png"
 import Link from 'next/link'
+import { getPages } from '@/sanity/sanity-utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,16 +11,29 @@ export const metadata = {
   description: 'Eric Doering Portfolio',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const pages = await getPages()
+
   return (
     <html lang="en">
       <body className="max-w-3xl mx-auto py-10">
-        <header>
-          <Link href="/" className="bg-gray-500 text-lg font-bold">Eric</Link>
+        <header className="flex items-center justify-between">
+            <Link href="/" className="bg-gray-500 text-lg font-bold">
+              Eric
+            </Link>
+              <div className="flex items-center gap-5 text-sm text-gray-600">
+                {pages.map((page) => (
+                  <Link key={page._id} href={`/${page.slug}`} className="hover:underline">
+                    {page.title}
+                  </Link>
+                ))}
+
+              </div>
         </header>
         <main className="py-20">{children}</main>
         </body>
--- eric_doering_website/sanity/sanity-utils.ts
+++ eric_doering_website/sanity/sanity-utils.ts
@@ -1,5 +1,6 @@
 import { createClient, groq } from "next-sanity";
 import { Project } from "@/types/Project";
+import { Page } from "@/types/Page"
 import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
@@ -33,4 +34,30 @@ export async function getProject(slug: string): Promise<Project>{
         { slug }
     ) 
 
+};
+
+
+export async function getPages(): Promise<Page[]>{
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "page"]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+        }`
+    )
+};
+
+
+export async function getPage(slug: string): Promise<Page>{
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "page" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+            content
+        }`,
+        { slug }
+    )
 };
